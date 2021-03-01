0564568a7640c6fb9047867c153df6a7ad090d72
masahito1997 <velonica.gale2227@gmail.com>
2021-03-01 18:26:55 +0900

    fix rspec for queue

--- ruby/spec/data_structures/queue/queue_spec.rb
+++ ruby/spec/data_structures/queue/queue_spec.rb
@@ -1,81 +1,60 @@
-    
 describe Queue do
+  let(:queue) { Queue.new }
+
   it 'should create empty queue' do
-    queue = Queue.new()
-    expect(queue).not.toBeNull();
-
-    expect(queue.linkedList).not.toBeNull();
-
+    expect(queue).not_to be eq nil
+    expect(queue.linked_list).not_to eq nil
   end
 
-    
+
   it 'should enqueue data to queue' do
-    queue = Queue.new()
-    
-    queue.enqueue(1);
+    queue.enqueue(1)
+    queue.enqueue(2)
 
-    queue.enqueue(2);
-
-    
     expect(queue.to_s).to eq '1,2'
   end
 
-    
+
   it 'should be possible to enqueue/dequeue objects' do
-    queue = Queue.new()
-    
- end
+    queue.enqueue({ value: 'test1', key: 'key1' })
+    queue.enqueue({ value: 'test2', key: 'key2' })
 
- end
+    expected = queue.to_s { |value| "#{value[:key]}:#{value[:value]}" }
 
-    
-    stringifier = (value) => `${value.key}:${value.value}`
-    
-    expect(queue.toString(stringifier)).to eq 'key1:test1,key2:test2'
-    expect(queue.dequeue().value).to eq 'test1'
-    expect(queue.dequeue().value).to eq 'test2'
+    expect(expected).to eq 'key1:test1,key2:test2'
+    expect(queue.dequeue[:value]).to eq 'test1'
+    expect(queue.dequeue[:value]).to eq 'test2'
   end
 
-    
+
   it 'should peek data from queue' do
-    queue = Queue.new()
-    
-    expect(queue.peek()).to eq nil
-    
-    queue.enqueue(1);
+    expect(queue.peek).to eq nil
 
-    queue.enqueue(2);
+    queue.enqueue(1)
+    queue.enqueue(2)
 
-    
-    expect(queue.peek()).to eq 1
-    expect(queue.peek()).to eq 1
+    expect(queue.peek).to eq 1
+    expect(queue.peek).to eq 1
   end
 
-    
+
   it 'should check if queue is empty' do
-    queue = Queue.new()
-    
-    expect(queue.isEmpty()).to eq true
-    
-    queue.enqueue(1);
+    expect(queue.empty?).to eq true
 
-    
-    expect(queue.isEmpty()).to eq false
+    queue.enqueue(1)
+
+    expect(queue.empty?).to eq false
   end
 
-    
+
   it 'should dequeue from queue in FIFO order' do
-    queue = Queue.new()
-    
-    queue.enqueue(1);
+    queue.enqueue(1)
+    queue.enqueue(2)
 
-    queue.enqueue(2);
-
-    
-    expect(queue.dequeue()).to eq 1
-    expect(queue.dequeue()).to eq 2
-    expect(queue.dequeue()).to eq nil
-    expect(queue.isEmpty()).to eq true
+    expect(queue.dequeue).to eq 1
+    expect(queue.dequeue).to eq 2
+    expect(queue.dequeue).to eq nil
+    expect(queue.empty?).to eq true
   end
 
 end
