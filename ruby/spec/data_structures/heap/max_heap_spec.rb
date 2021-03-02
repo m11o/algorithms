c7d35ce466b0cd2baf5b048d1cffd6a4e22ba06b
masahito1997 <velonica.gale2227@gmail.com>
2021-03-02 18:26:25 +0900

    fix max_heap_spec.rb

--- ruby/spec/data_structures/heap/max_heap_spec.rb
+++ ruby/spec/data_structures/heap/max_heap_spec.rb
@@ -1,221 +1,141 @@
-    
 xdescribe MaxHeap do
+  let(:max_heap) { MaxHeap.new }
+
   it 'should create an empty max heap' do
-    maxHeap = MaxHeap.new()
-    
-    expect(maxHeap.peek()).to eq nil
-    expect(maxHeap.isEmpty()).to eq true
+    expect(max_heap.peek).to eq nil
+    expect(max_heap.empty?).to eq true
   end
 
-    
   it 'should add items to the heap and heapify it up' do
-    maxHeap = MaxHeap.new()
-    
-    maxHeap.add(5);
+    max_heap.add(5)
 
-    expect(maxHeap.isEmpty()).to eq false
-    expect(maxHeap.peek()).to eq 5
-    expect(maxHeap.to_s).to eq '5'
-    
-    maxHeap.add(3);
+    expect(max_heap.empty?).to eq false
+    expect(max_heap.peek).to eq 5
+    expect(max_heap.to_s).to eq '5'
 
-    expect(maxHeap.peek()).to eq 5
-    expect(maxHeap.to_s).to eq '5,3'
-    
-    maxHeap.add(10);
+    max_heap.add(3)
 
-    expect(maxHeap.peek()).to eq 10
-    expect(maxHeap.to_s).to eq '10,3,5'
-    
-    maxHeap.add(1);
+    expect(max_heap.peek).to eq 5
+    expect(max_heap.to_s).to eq '5,3'
 
-    expect(maxHeap.peek()).to eq 10
-    expect(maxHeap.to_s).to eq '10,3,5,1'
-    
-    maxHeap.add(1);
+    max_heap.add(10)
 
-    expect(maxHeap.peek()).to eq 10
-    expect(maxHeap.to_s).to eq '10,3,5,1,1'
-    
-    expect(maxHeap.poll()).to eq 10
-    expect(maxHeap.to_s).to eq '5,3,1,1'
-    
-    expect(maxHeap.poll()).to eq 5
-    expect(maxHeap.to_s).to eq '3,1,1'
-    
-    expect(maxHeap.poll()).to eq 3
-    expect(maxHeap.to_s).to eq '1,1'
+    expect(max_heap.peek).to eq 10
+    expect(max_heap.to_s).to eq '10,3,5'
+
+    max_heap.add(1)
+
+    expect(max_heap.peek).to eq 10
+    expect(max_heap.to_s).to eq '10,3,5,1'
+
+    max_heap.add(1)
+
+    expect(max_heap.peek).to eq 10
+    expect(max_heap.to_s).to eq '10,3,5,1,1'
+
+    expect(max_heap.poll).to eq 10
+    expect(max_heap.to_s).to eq '5,3,1,1'
+
+    expect(max_heap.poll).to eq 5
+    expect(max_heap.to_s).to eq '3,1,1'
+
+    expect(max_heap.poll).to eq 3
+    expect(max_heap.to_s).to eq '1,1'
   end
 
-    
   it 'should poll items from the heap and heapify it down' do
-    maxHeap = MaxHeap.new()
-    
-    maxHeap.add(5);
+    max_heap.add(5)
+    max_heap.add(3)
+    max_heap.add(10)
+    max_heap.add(11)
+    max_heap.add(1)
 
-    maxHeap.add(3);
+    expect(max_heap.to_s).to eq '11,10,5,3,1'
 
-    maxHeap.add(10);
+    expect(max_heap.poll).to eq 11
+    expect(max_heap.to_s).to eq '10,3,5,1'
 
-    maxHeap.add(11);
+    expect(max_heap.poll).to eq 10
+    expect(max_heap.to_s).to eq '5,3,1'
 
-    maxHeap.add(1);
+    expect(max_heap.poll).to eq 5
+    expect(max_heap.to_s).to eq '3,1'
 
-    
-    expect(maxHeap.to_s).to eq '11,10,5,3,1'
-    
-    expect(maxHeap.poll()).to eq 11
-    expect(maxHeap.to_s).to eq '10,3,5,1'
-    
-    expect(maxHeap.poll()).to eq 10
-    expect(maxHeap.to_s).to eq '5,3,1'
-    
-    expect(maxHeap.poll()).to eq 5
-    expect(maxHeap.to_s).to eq '3,1'
-    
-    expect(maxHeap.poll()).to eq 3
-    expect(maxHeap.to_s).to eq '1'
-    
-    expect(maxHeap.poll()).to eq 1
-    expect(maxHeap.to_s).to eq ''
-    
-    expect(maxHeap.poll()).to eq nil
-    expect(maxHeap.to_s).to eq ''
+    expect(max_heap.poll).to eq 3
+    expect(max_heap.to_s).to eq '1'
+
+    expect(max_heap.poll).to eq 1
+    expect(max_heap.to_s).to eq ''
+
+    expect(max_heap.poll).to eq nil
+    expect(max_heap.to_s).to eq ''
   end
 
-    
   it 'should heapify down through the right branch as well' do
-    maxHeap = MaxHeap.new()
-    
-    maxHeap.add(3);
+    max_heap.add(3)
+    max_heap.add(12)
+    max_heap.add(10)
 
-    maxHeap.add(12);
+    expect(max_heap.to_s).to eq '12,3,10'
 
-    maxHeap.add(10);
+    max_heap.add(11)
 
-    
-    expect(maxHeap.to_s).to eq '12,3,10'
-    
-    maxHeap.add(11);
+    expect(max_heap.to_s).to eq '12,11,10,3'
 
-    expect(maxHeap.to_s).to eq '12,11,10,3'
-    
-    expect(maxHeap.poll()).to eq 12
-    expect(maxHeap.to_s).to eq '11,3,10'
+    expect(max_heap.poll).to eq 12
+    expect(max_heap.to_s).to eq '11,3,10'
   end
 
-    
   it 'should be possible to find item indices in heap' do
-    maxHeap = MaxHeap.new()
-    
-    maxHeap.add(3);
+    max_heap.add(3)
+    max_heap.add(12)
+    max_heap.add(10)
+    max_heap.add(11)
+    max_heap.add(11)
 
-    maxHeap.add(12);
+    expect(max_heap.to_s).to eq '12,11,10,3,11'
 
-    maxHeap.add(10);
-
-    maxHeap.add(11);
-
-    maxHeap.add(11);
-
-    
-    expect(maxHeap.to_s).to eq '12,11,10,3,11'
-    
-    expect(maxHeap.find(5)).to eq []
-    expect(maxHeap.find(12)).to eq [0]
-    expect(maxHeap.find(11)).to eq [1, 4]
+    expect(max_heap.find(5)).to eq []
+    expect(max_heap.find(12)).to eq [0]
+    expect(max_heap.find(11)).to eq [1, 4]
   end
 
-    
   it 'should be possible to remove items from heap with heapify down' do
-    maxHeap = MaxHeap.new()
-    
-    maxHeap.add(3);
+    max_heap.add(3)
+    max_heap.add(12)
+    max_heap.add(10)
+    max_heap.add(11)
+    max_heap.add(11)
 
-    maxHeap.add(12);
+    expect(max_heap.to_s).to eq '12,11,10,3,11'
 
-    maxHeap.add(10);
-
-    maxHeap.add(11);
-
-    maxHeap.add(11);
-
-    
-    expect(maxHeap.to_s).to eq '12,11,10,3,11'
-    
-    expect(maxHeap.remove(12).to eq '11,11,10,3'
-    expect(maxHeap.remove(12).peek()).to eq 11
-    expect(maxHeap.remove(11).to eq '10,3'
-    expect(maxHeap.remove(10).peek()).to eq 3
+    expect(max_heap.remove(12)).to eq '11,11,10,3'
+    expect(max_heap.remove(12).peek).to eq 11
+    expect(max_heap.remove(11)).to eq '10,3'
+    expect(max_heap.remove(10).peek).to eq 3
   end
 
-    
   it 'should be possible to remove items from heap with heapify up' do
-    maxHeap = MaxHeap.new()
-    
-    maxHeap.add(3);
+    max_heap.add(3)
+    max_heap.add(10)
+    max_heap.add(5)
+    max_heap.add(6)
+    max_heap.add(7)
+    max_heap.add(4)
+    max_heap.add(6)
+    max_heap.add(8)
+    max_heap.add(2)
+    max_heap.add(1)
 
-    maxHeap.add(10);
-
-    maxHeap.add(5);
-
-    maxHeap.add(6);
-
-    maxHeap.add(7);
-
-    maxHeap.add(4);
-
-    maxHeap.add(6);
-
-    maxHeap.add(8);
-
-    maxHeap.add(2);
-
-    maxHeap.add(1);
-
-    
-    expect(maxHeap.to_s).to eq '10,8,6,7,6,4,5,3,2,1'
-    expect(maxHeap.remove(4).to eq '10,8,6,7,6,1,5,3,2'
-    expect(maxHeap.remove(3).to eq '10,8,6,7,6,1,5,2'
-    expect(maxHeap.remove(5).to eq '10,8,6,7,6,1,2'
-    expect(maxHeap.remove(10).to eq '8,7,6,2,6,1'
-    expect(maxHeap.remove(6).to eq '8,7,1,2'
-    expect(maxHeap.remove(2).to eq '8,7,1'
-    expect(maxHeap.remove(1).to eq '8,7'
-    expect(maxHeap.remove(7).to eq '8'
-    expect(maxHeap.remove(8).to eq ''
+    expect(max_heap.to_s).to eq '10,8,6,7,6,4,5,3,2,1'
+    expect(max_heap.remove(4)).to eq '10,8,6,7,6,1,5,3,2'
+    expect(max_heap.remove(3)).to eq '10,8,6,7,6,1,5,2'
+    expect(max_heap.remove(5)).to eq '10,8,6,7,6,1,2'
+    expect(max_heap.remove(10)).to eq '8,7,6,2,6,1'
+    expect(max_heap.remove(6)).to eq '8,7,1,2'
+    expect(max_heap.remove(2)).to eq '8,7,1'
+    expect(max_heap.remove(1)).to eq '8,7'
+    expect(max_heap.remove(7)).to eq '8'
+    expect(max_heap.remove(8)).to eq ''
   end
-
-    
-  it 'should be possible to remove items from heap with custom finding comparator' do
-    maxHeap = MaxHeap.new()
-    maxHeap.add('a');
-
-    maxHeap.add('bb');
-
-    maxHeap.add('ccc');
-
-    maxHeap.add('dddd');
-
-    
-    expect(maxHeap.to_s).to eq 'dddd,ccc,bb,a'
-    
-    comparator = Comparator.new((a, b)
-    if (a.length === b.length) {
-
-    return 0;
-
-    }
-
-    
-    return a.length < b.length ? -1 : 1;
-
-    end
-
-    
-    maxHeap.remove('hey', comparator);
-
-    expect(maxHeap.to_s).to eq 'dddd,a,bb'
-  end
-
 end
