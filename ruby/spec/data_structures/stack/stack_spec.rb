d8ccef5cb865cbdcd8fe378608cf046ff8e36cdd
masahito1997 <velonica.gale2227@gmail.com>
2021-03-02 14:56:11 +0900

    fix rspec

--- ruby/spec/data_structures/stack/stack_spec.rb
+++ ruby/spec/data_structures/stack/stack_spec.rb
@@ -1,97 +1,69 @@
-    
-xdescribe Stack do
+describe Stack do
+  let(:stack) { Stack.new }
+
   it 'should create empty stack' do
-    stack = Stack.new()
-    expect(stack).not.toBeNull();
-
-    expect(stack.linkedList).not.toBeNull();
-
+    expect(stack).not_to eq nil
+    expect(stack.linked_list).not_to eq nil
   end
 
-    
+
   it 'should stack data to stack' do
-    stack = Stack.new()
-    
-    stack.push(1);
+    stack.push(1)
+    stack.push(2)
 
-    stack.push(2);
-
-    
     expect(stack.to_s).to eq '2,1'
   end
 
-    
+
   it 'should peek data from stack' do
-    stack = Stack.new()
-    
-    expect(stack.peek()).to eq nil
-    
-    stack.push(1);
+    expect(stack.peek).to eq nil
 
-    stack.push(2);
+    stack.push(1)
+    stack.push(2)
 
-    
-    expect(stack.peek()).to eq 2
-    expect(stack.peek()).to eq 2
+    expect(stack.peek).to eq 2
+    expect(stack.peek).to eq 2
   end
 
-    
+
   it 'should check if stack is empty' do
-    stack = Stack.new()
-    
-    expect(stack.isEmpty()).to eq true
-    
-    stack.push(1);
+    expect(stack.empty?).to eq true
 
-    
-    expect(stack.isEmpty()).to eq false
+    stack.push(1)
+
+    expect(stack.empty?).to eq false
   end
 
-    
+
   it 'should pop data from stack' do
-    stack = Stack.new()
-    
-    stack.push(1);
+    stack.push(1)
+    stack.push(2)
 
-    stack.push(2);
-
-    
-    expect(stack.pop()).to eq 2
-    expect(stack.pop()).to eq 1
-    expect(stack.pop()).to eq nil
-    expect(stack.isEmpty()).to eq true
+    expect(stack.pop).to eq 2
+    expect(stack.pop).to eq 1
+    expect(stack.pop).to eq nil
+    expect(stack.empty?).to eq true
   end
 
-    
+
   it 'should be possible to push/pop objects' do
-    stack = Stack.new()
-    
- end
+    stack.push({ value: 'test1', key: 'key1' })
+    stack.push({ value: 'test2', key: 'key2' })
 
- end
-
-    
-    stringifier = (value) => `${value.key}:${value.value}`
-    
-    expect(stack.toString(stringifier)).to eq 'key2:test2,key1:test1'
-    expect(stack.pop().value).to eq 'test2'
-    expect(stack.pop().value).to eq 'test1'
+    expected = stack.to_s { |value| "#{value[:key]}:#{value[:value]}" }
+    expect(expected).to eq 'key2:test2,key1:test1'
+    expect(stack.pop[:value]).to eq 'test2'
+    expect(stack.pop[:value]).to eq 'test1'
   end
 
-    
+
   it 'should be possible to convert stack to array' do
-    stack = Stack.new()
-    
-    expect(stack.peek()).to eq nil
-    
-    stack.push(1);
+    expect(stack.peek).to eq nil
 
-    stack.push(2);
+    stack.push(1)
+    stack.push(2)
+    stack.push(3)
 
-    stack.push(3);
-
-    
-    expect(stack.toArray()).to eq [3, 2, 1]
+    expect(stack.to_a).to eq [3, 2, 1]
   end
-
 end
