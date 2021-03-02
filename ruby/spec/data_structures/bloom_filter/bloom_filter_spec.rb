981a5e062537ad27993f546e9005e173bb0715a1
masahito1997 <velonica.gale2227@gmail.com>
2021-03-02 17:19:23 +0900

    fix rubocop for bloom_filter_spec.rb

--- ruby/spec/data_structures/bloom_filter/bloom_filter_spec.rb
+++ ruby/spec/data_structures/bloom_filter/bloom_filter_spec.rb
@@ -1,16 +1,15 @@
 xdescribe BloomFilter do
-  PEOPLE = ['Bruce Wayne', 'Clark Kent', 'Barry Allen'].freeze
+  people = %w[Bruce\ Wayne Clark\ Kent Barry\ Allen].freeze
 
   let(:bloom_filter) { BloomFilter.new }
 
-  it 'should create a new filter store with the appropriate methods' do
+  it 'creates a new filter store with the appropriate methods' do
     # store = bloom_filter.create_store(18)
     # expect(typeof store.getValue).to eq 'function'
     # expect(typeof store.setValue).to eq 'function'
   end
 
-
-  it 'should hash deterministically with all 3 hash functions' do
+  it 'hashes deterministically with all 3 hash functions' do
     str1 = 'apple'
 
     expect(bloom_filter.hash1(str1)).to eq bloom_filter.hash1(str1)
@@ -32,15 +31,13 @@ xdescribe BloomFilter do
     expect(bloom_filter.hash3(str2)).to eq 10
   end
 
-
-  it 'should create an array with 3 hash values' do
+  it 'creates an array with 3 hash values' do
     expect(bloom_filter.hash_values('abc').length).to eq 3
     expect(bloom_filter.hash_values('abc')).to eq [66, 63, 54]
   end
 
-
-  it 'should insert strings correctly and return true when checking for inserted values' do
-    PEOPLE.each { |person| bloom_filter.insert(person) }
+  it 'inserts strings correctly and return true when checking for inserted values' do
+    people.each { |person| bloom_filter.insert(person) }
 
     expect(bloom_filter.include?('Bruce Wayne')).to eq true
     expect(bloom_filter.include?('Clark Kent')).to eq true
@@ -48,5 +45,4 @@ xdescribe BloomFilter do
 
     expect(bloom_filter.include?('Tony Stark')).to eq false
   end
-
 end
