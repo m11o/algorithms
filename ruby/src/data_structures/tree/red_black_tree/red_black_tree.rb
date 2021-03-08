826534892b573541f2a08c6abd51e5aa6aa356ab
masahito1997 <velonica.gale2227@gmail.com>
2021-03-08 18:40:03 +0900

    test commit

--- ruby/src/data_structures/tree/red_black_tree/red_black_tree.rb
+++ ruby/src/data_structures/tree/red_black_tree/red_black_tree.rb
@@ -1,5 +1,6 @@
 require_relative '../binary_search_tree/binary_search_tree'
 
+# これむずくね？(git commitテスト)
 class RedBlackTree < BinarySearchTree
   COLOR_PROP_NAME = 'color'.freeze
   RED_BLACK_TREE_COLORS = {
